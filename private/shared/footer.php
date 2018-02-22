da5c23c18940f0486e0ae9155c601a7319318014
BadButNotBoujee <shayneah@googlemail.com>
2018-02-22 19:10:38 +0000

    Add placeholders for beta functions

--- private/shared/footer.php
+++ private/shared/footer.php
@@ -1,4 +1,3 @@
-<?php require_once("../initialize.php"); ?>
 
 <nav class="navbar navbar-bottom bg-faded " style="background-color: #e3f2fd;">
     <img src=<?php echo WROOT."./ShayneOSLogo.png"?> width="50" height="50" class="d-inline-block align-top" alt="">
