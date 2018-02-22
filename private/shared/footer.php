3a33fc256328eea7dd6fc1658f8d0e062bbc27c9
BadButNotBoujee <shayneah@googlemail.com>
2018-02-22 21:31:10 +0000

    Add background

--- private/shared/footer.php
+++ private/shared/footer.php
@@ -1,6 +1,6 @@
 <div class="container-fluid">
 <nav class="navbar navbar-bottom bg-faded " style="background-color: #e3f2fd;">
-    <img src=<?php echo WROOT."./ShayneOSLogo.png"?> width="50" height="50" class="d-inline-block align-top" alt="">
+    <img src=<?php echo "./ShayneOSLogo.png"?> width="50" height="50" class="d-inline-block align-top" alt="">
 	<p>  Shayne Ahchoon &nbsp <br>
   
      &copy ShayneOS <br>
