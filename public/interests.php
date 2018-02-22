7e07aed41a699568556cb4547b550de1f7d7af40
BadButNotBoujee <shayneah@googlemail.com>
2018-02-22 16:56:48 +0000

    Add upload reminder functionality and database queries

--- public/interests.php
+++ public/interests.php
@@ -1,8 +1,20 @@
-  <div class="card" style="margin: 1em 1em 1em 1em;">
-            <div class="card-header ">Areas of interest <button class="btn btn-success float-right">Add Interest</button></div>
+
+<div class="card" style="margin: 1em 1em 1em 1em;">
+            <div class="card-header" style="color: black; background-color: #ffb3b3">Interests <button class="btn btn-success float-right" data-toggle="collapse" data-target="#interests-form">Add Interest</button></div>
           
             <div class="card-body">
-					  <?php    
+                
+                <div id="interests-form" class="collapse">
+                    <form action="process_interests.php" method="POST">
+                     <input class="form-control" type="text" placeholder="Title" name="interest-title"><br>
+                    <textarea class="form-control" name="interest-body" placeholder="Details" rows="3"></textarea><br>
+                    <button class="btn btn-success float-right">Submit</button><br><br>
+                    <hr>
+                     </form>
+                          
+                </div>
+                
+                  <?php    
                         
                         $connection = makeConnection();
                         $stat = pg_connection_status($connection);
@@ -18,9 +30,7 @@
                        
                      pg_close($connection);  
                     
-                    ?>            
-            
+                    ?>
+                    
             </div>
-                
-          </div>
-          
+</div>
